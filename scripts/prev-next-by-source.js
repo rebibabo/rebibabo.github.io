3acf18f846bc9edd47661d46a87d7fda00fdf1a4
rebibabo <2841329980@qq.com>
2026-07-07 11:45:06 +0800

    vault backup: 2026-07-07 11:45:06

--- scripts/prev-next-by-source.js
+++ scripts/prev-next-by-source.js
@@ -1,24 +1,38 @@
 /* global hexo */
 'use strict';
 
-var through2 = require('through2');
+hexo.extend.filter.register('after_init', function() {
+  // After init, replace the built-in post generator to sort by source
+  // The original post.js generator does: locals.posts.sort('-date').toArray()
+  // We re-register and rely on our version running after the built-in one
 
-// Hook into Hexo's Router to inject prev/next URLs based on source order
+  // Actually, directly monkey-patch the built-in post generator module
+  // to change '-date' to 'source'
+  try {
+    var postGenPath = require.resolve('hexo/dist/plugins/generator/post');
+    delete require.cache[postGenPath];
+    var genModule = require(postGenPath);
+    // We can't easily patch this, so let's try another way
+  } catch(e) {}
+});
+
+// Cleanest approach: intercept at the locals level
+// Override how locals.posts.sort works to always sort by source
 hexo.extend.filter.register('before_generate', function() {
-  // Get all posts sorted by source
-  var posts = hexo.locals.get('posts').data;
-  if (!posts || posts.length < 2) return;
+  var origGet = hexo.locals.get.bind(hexo.locals);
 
-  posts.sort(function(a, b) {
-    return a.source.localeCompare(b.source);
-  });
+  // Store sorted posts array so everyone gets the same order
+  var sortedPosts = null;
 
-  // Directly set prev/next on each post so the theme's helpers pick them up
-  for (var i = 0; i < posts.length; i++) {
-    if (i > 0) posts[i].prev = posts[i - 1];
-    else posts[i].prev = null;
-
-    if (i < posts.length - 1) posts[i].next = posts[i + 1];
-    else posts[i].next = null;
-  }
+  // Monkey-patch hexo.locals.get to wrap 'posts'
+  hexo.locals.get = function(name) {
+    var result = origGet(name);
+    if (name === 'posts' && result) {
+      if (!sortedPosts) {
+        sortedPosts = result.sort('source');
+      }
+      return sortedPosts;
+    }
+    return result;
+  };
 });
